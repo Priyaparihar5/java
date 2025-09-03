6f02bc34d0c5445c991bf21ede20ef9111e149aa
Priya Parihar <priyaparihar1605@gmail.com>
2025-09-03 22:23:57 +0530

    assignment

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,11 @@
+public class bitwise operator {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 3;
+        System.out.println("a & b = " + ( a & b));
+        System.out.println("a | b = " + ( a | b));
+        System.out.println("a ^ b = " + ( a ^ b));
+        System.out.println("a << 1 = " + ( a << b));
+        System.out.println("a >> 1 = " + ( a >> b));
+    }
+}
